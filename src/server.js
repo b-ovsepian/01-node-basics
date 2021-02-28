9e5bc1c618350fce45b25367932718386f5a5c93
Bagdasar Ovsepyan <bagdasar@ovsepyan.com.ua>
2021-02-28 15:25:53 +0200

    hw05

--- src/server.js
+++ src/server.js
@@ -38,6 +38,7 @@ export class ContactsServer {
     try {
       await mongoose.connect(process.env.MONGODB_URL, {
         useNewUrlParser: true,
+        useCreateIndex: true,
         useUnifiedTopology: true,
         useFindAndModify: false,
       });
